c8ae8ee565a1208dddfdeaf297797cd9cfa69958
Brian <mahuibrian14@gmail.com>
2023-06-21 13:28:54 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ Student.create(first_name: 'Luke', last_name: 'Oigara', grade: 10)
+ puts "Done seeding"
